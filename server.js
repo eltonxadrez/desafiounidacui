c6ac1f6c7ffa978c32f6e73c87e693e9b955fd72
Elton J <eltonjanduhy@gmail.com>
2021-02-26 11:48:16 -0300

    configuração 1.2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/desafio-unidac-ui'));
+app.use(express.static('./dist/desafiounidacui'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/desafio-unidac-ui/'}),
+    res.sendFile('index.html', {root: 'dist/desafiounidacui/'}),
 );
 
 // Start the app by listening on the default Heroku port
