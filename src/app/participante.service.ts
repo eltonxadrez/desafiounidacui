bc7a11a8dca50151b7b62306247e4dc3b24a09c0
Elton J <eltonjanduhy@gmail.com>
2021-02-26 09:48:02 -0300

    correção de bug

--- src/app/participante.service.ts
+++ src/app/participante.service.ts
@@ -6,18 +6,7 @@ export class ParticipanteService {
 
   constructor(private http: HttpClient) {}
 
-  consultar(){
 
-    this.http.get('https://desafio-unidac-apps.herokuapp.com/colaboradores')
-
-    .toPromise()
-    .then(response => {
-      console.log(response)
-    }
-
-    )
-
-  }
 
 }
 
